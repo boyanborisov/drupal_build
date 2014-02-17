4e9076f6b0785c01fa3f19415a181c86712f22f2
Boyan Borisov <boyanb@propeople.dk>
2014-02-17 15:57:11 +0200

    Init drupal build repo

---
+++ settings/drushrc.php
@@ -0,0 +1,5 @@
+<?php
+
+// drush language - engilsh
+$options['variables']['language_default'] = (object) array('language' => 'en', 'name' => 'English', 'native' => 'English', 'direction' => 0, 'enabled' => 1, 'plurals' => 0, 'formula' => '', 'domain' => '', 'prefix' => '', 'weight' => 0, 'javascript' => '');
+
